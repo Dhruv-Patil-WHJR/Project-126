de21a4a4ee03c528bd906a8c5d73b98dcc6cc4c9
Dhruv-Patil-WHJR <122462051+Dhruv-Patil-WHJR@users.noreply.github.com>
2023-04-21 19:48:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+peter_pan_song = ""
+harry_potter_song = ""
+
+function setup()
+{
+    canvas = createCanvas(500,600);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
